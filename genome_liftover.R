6b4126a77fb0b04f0f7810f6ab734747759d1bec
Qian Liu <qliu7@buffalo.edu>
2022-10-27 12:04:33 -0400

    add recipe for genome liftover

---
+++ genome_liftover.R
@@ -0,0 +1,9 @@
+script <- "
+from=$1
+to=$2
+wget https://ftp.ensembl.org/pub/assembly_mapping/homo_sapiens/${from}_to_${to}.chain.gz"
+rcp_liftover <- recipeMake(shscript = script,
+                           paramID = c("from", "to"), 
+                           paramType = c("string", "string"), 
+                           outputID = "genome_lift_over", 
+                           outputGlob = "*.chain.gz")  
