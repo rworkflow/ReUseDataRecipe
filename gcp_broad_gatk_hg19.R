3e8084a57472a3b44009e583b04109d440acd363
Qian Liu <qliu7@buffalo.edu>
2022-12-09 14:03:48 -0500

    update tcga gcp recipes

--- gcp_broad_gatk_hg19.R
+++ gcp_broad_gatk_hg19.R
@@ -13,23 +13,21 @@ gcp_broad_gatk_hg19 <- recipeMake(shscript = script,
                                   outputID = "gfile",
                                   outputGlob = "$(inputs.filename)*")
 
-gcp_broad_gatk_hg19 <- addMeta(gcp_broad_gatk_hg19,
-                               label = "gcp_broad_gatk_hg19",
-                               doc = "")
-
-gcp_broad_gatk_hg19 <- addMeta(gcp_broad_gatk_hg19,
-                               label = "GCP_broad_gatk_hg19",
-                               doc = "GATK annotation bundle for hg19 from GCP (gs://gcp-public-data--broad-references/hg19/v0/)",
-                               inputLabels = c("file name", "index"),
-                               inputDocs = c("The file basename to download from the broad GCP bucket",
-                                             "The 'idx' or 'tbi' index file if existing"),
-                               outputLabels = c("gfile"),
-                               outputDocs = c("The downloaded annotation files"),
-                               extensions = list(author = "rworkflow team",
-                                                 date = Sys.Date(),
-                                                 url = "https://storage.googleapis.com/gcp-public-data--broad-references/hg19/v0/",
-                                                 example = paste("rcp <- recipeLoad('gcp_broad_gatk_hg19')",
-                                                                 "rcp$filename <- '1000G_omni2.5.b37.vcf.gz'",
-                                                                 "getData(rcp, outdir = 'data/folder', notes = c('gcp', 'broad', 'reference', 'hg19', 'v0', '1000G', 'omni2.5')",
-                                                                 sep="\n"))
-                               )
+gcp_broad_gatk_hg19 <- addMeta(
+    gcp_broad_gatk_hg19,
+    label = "GCP_broad_gatk_hg19",
+    doc = "GATK annotation bundle for hg19 from GCP (gs://gcp-public-data--broad-references/hg19/v0/)",
+    inputLabels = c("file name", "index"),
+    inputDocs = c("The file basename to download from the broad GCP bucket",
+                  "The 'idx' or 'tbi' index file if existing"),
+    outputLabels = c("gfile"),
+    outputDocs = c("The downloaded annotation files"),
+    extensions = list(author = "rworkflow team",
+                      date = Sys.Date(),
+                      url = "https://console.cloud.google.com/storage/browser/gcp-public-data--broad-references/hg19/v0",
+                      example = paste(
+                          "rcp <- recipeLoad('gcp_broad_gatk_hg19')",
+                          "rcp$filename <- '1000G_omni2.5.b37.vcf.gz'",
+                          "getData(rcp, outdir = 'data/folder', notes = c('gcp', 'broad', 'reference', 'hg19', 'v0', '1000G', 'omni2.5')",
+                          sep="\n"))
+)
--- gcp_broad_gatk_hg38.R
+++ gcp_broad_gatk_hg38.R
@@ -1,4 +1,3 @@
-
 script <- "
 filename=$1
 idx=$2
@@ -13,23 +12,21 @@ gcp_broad_gatk_hg38 <- recipeMake(shscript = script,
                                   outputID = "gfile",
                                   outputGlob = "$(inputs.filename)*")
 
-gcp_broad_gatk_hg38 <- addMeta(gcp_broad_gatk_hg38,
-                               label = "gcp_broad_gatk_hg38",
-                               doc = "")
-
-gcp_broad_gatk_hg38 <- addMeta(gcp_broad_gatk_hg38,
-                               label = "GCP_broad_gatk_hg38",
-                               doc = "GATK annotation bundle for hg38 from GCP (gs://gcp-public-data--broad-references/hg38/v0/)",
-                               inputLabels = c("file name", "index"),
-                               inputDocs = c("The file basename to download from the broad GCP bucket",
-                                             "The 'idx' or 'tbi' index file if existing"),
-                               outputLabels = c("gfile"),
-                               outputDocs = c("The downloaded annotation files"),
-                               extensions = list(author = "rworkflow team",
-                                                 date = Sys.Date(),
-                                                 url = "https://storage.googleapis.com/gcp-public-data--broad-references/hg38/v0/",
-                                                 example = paste("rcp <- recipeLoad('gcp_broad_gatk_hg38')",
-                                                                 "rcp$filename <- '1000G_omni2.5.hg38.vcf.gz'",
-                                                                 "getData(rcp, outdir = 'data/folder', notes = c('gcp', 'broad', 'reference', 'hg38', 'v0', '1000G', 'omni2.5')",
-                                                                 sep="\n"))
-                               )
+gcp_broad_gatk_hg38 <- addMeta(
+    gcp_broad_gatk_hg38,
+    label = "GCP_broad_gatk_hg38",
+    doc = "GATK annotation bundle for hg38 from GCP (gs://gcp-public-data--broad-references/hg38/v0/)",
+    inputLabels = c("file name", "index"),
+    inputDocs = c("The file basename to download from the broad GCP bucket",
+                  "The 'idx' or 'tbi' index file if existing"),
+    outputLabels = c("gfile"),
+    outputDocs = c("The downloaded annotation files"),
+    extensions = list(
+        author = "rworkflow team",
+        date = Sys.Date(),
+        url = "https://console.cloud.google.com/storage/browser/gcp-public-data--broad-references/hg38/v0",
+        example = paste("rcp <- recipeLoad('gcp_broad_gatk_hg38')",
+                        "rcp$filename <- '1000G_omni2.5.hg38.vcf.gz'",
+                        "getData(rcp, outdir = 'data/folder', notes = c('gcp', 'broad', 'reference', 'hg38', 'v0', '1000G', 'omni2.5')",
+                        sep="\n"))
+)
